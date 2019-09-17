9de33cfcd069c819db9cf2e1dca7ee1e5fd5c85d
18774209212 <2485408442@qq.com>
2019-09-17 17:51:13 +0800

    页面渲染

--- src/main.js
+++ src/main.js
@@ -2,16 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import store from './vuex/store'
 import router from './router'
+import Router from 'vue-router'
+
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
 import 'lib-flexible'
 import elementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(elementUI)
+Vue.use({router,store})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
--- src/pages/info.vue
+++ src/pages/info.vue
@@ -1,7 +1,13 @@
 <!--游戏说明-->
 <template>
     <div class="page">
-      
+      <div class="info">
+          <p>游戏计时：1分钟</p>
+          <p>积分获取详情</p>
+          <p>辣椒：3分</p>
+          <p>苹果：2分</p>
+          <p>橙子：1分</p>
+      </div>
     </div>
 </template>
 <script>
@@ -16,9 +22,23 @@ export default {
 <style lang='less' scoped>
 .page{
     width:100%;
-    height: 1000px;
-    background-image: url('../../static/imgs/bg2.png');
+    height: 100%;
+    background-image: url('../../static/imgs/bg-info.jpg');
     background-size: 100% 100%;
     background-repeat: no-repeat;
+    position: relative;
+    .info{
+        position: absolute;
+        top: 33%;
+        left: 45%;
+        p{
+            padding: 0.2rem 0;
+            font-size: 0.45rem;
+            color: #ffffff;
+             text-shadow: -1px 0 rgb(61,38,12), 0 1px rgb(61,38,12),
+            1px 0 rgb(61,38,12), 0 -1px rgb(61,38,12);
+            font-weight: bold;
+        }
+    }
 }
 </style>
